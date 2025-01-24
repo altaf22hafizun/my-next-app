d4c245a0c18c572c8692b49d3a8fc2515acb6886
Altaf Hafizun <piizuunee@gmail.com>
2025-01-24 16:18:46 +0700

    next-my-app

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,13 +1,17 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./Navbar.module.css";
+import Link from "next/link";
 
 const Navbar = () => {
   const { data }: any = useSession();
 
   return (
     <div className={styles.navbar}>
-      <div className="big">Navbar</div>
+      <div className="big">
+        <Link href={"/"}>Navbar</Link>
+      </div>
       <div className="text-white">
+        <Link href={"/product"} className={styles.link}>Product</Link>
         {data && data.user.fullname}
         {data ? (
           <button className={styles.button} onClick={() => signOut()}>
